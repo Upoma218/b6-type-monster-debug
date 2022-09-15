b5c46dd5e116bb9847d57565ab5c0a5b4bdf8fdf
Resma Islam Upoma <upoma.cdsr@gmail.com>
2022-09-15 06:43:58 +0600

    eddition added

--- script.js
+++ script.js
@@ -68,7 +68,8 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTakenInt = (finishTime - startTime) / 1000;
+  const timeTaken = parseInt(timeTakenInt);
 
   // show result modal
   resultModal.innerHTML = "";
